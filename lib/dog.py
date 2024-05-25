ee60105fedca8598217ca5919109dbfad553df81
siyajari samuel <siyajari.samuel@student.moringaschool.com>
2024-05-25 15:33:19 +0300

    Object Initialization

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,12 @@
-#!/usr/bin/env python3
-
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+
+# Create an instance of the Dog class with only the name argument
+fido = Dog("fido")
+print(f"fido's breed: {fido.breed}")  
+
+# Create another instance of the Dog class with both name and breed arguments
+snoopy = Dog("snoopy", "Beagle")
+print(f"snoopy's breed: {snoopy.breed}")  
